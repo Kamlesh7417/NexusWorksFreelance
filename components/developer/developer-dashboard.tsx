685ce471e7495d2c096d2817831aac3dfd2b77b8
Kamlesh7417 <51952452+Kamlesh7417@users.noreply.github.com>
2025-06-15 23:36:47 +0530

    Updated developer-dashboard.tsx

--- components/developer/developer-dashboard.tsx
+++ components/developer/developer-dashboard.tsx
@@ -24,6 +24,7 @@ import {
 } from 'lucide-react';
 import Link from 'next/link';
 import { MessageList } from '../ui/message-list';
+import { Logo } from '@/components/ui/logo';
 
 interface DeveloperDashboardProps {
   user: any;
